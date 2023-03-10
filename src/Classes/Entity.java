0b56e5c6aef1bb26295e762915242566cb93bc2a
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2023-03-10 11:28:38 +0000

    Entity class

--- src/Classes/Entity.java
+++ src/Classes/Entity.java
@@ -1,5 +1,33 @@
 package Classes;
 
-public class Entity {
+import static Classes.EntityState.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Entity extends ImageView {
     
+    EntityState state;
+
+    // Pretty much just setting the state of the entity - location and status of it.
+
+    public Entity(double x, double y, Image img) {
+        this.setX(x);
+        this.setY(y);
+        this.state = ALIVE;
+        this.setImage(img);
+    }
+
+    void moveX (double velX) {
+        setX(getX()+velX); // Vel = Velocity
+    }
+
+    void moveY(double velY) {
+        setY(getY()+velY); 
+    }
+
+    boolean isDead() {
+        return state == DEAD;
+    }
+
+
 }
--- src/Utils/Images.java
+++ src/Utils/Images.java
@@ -8,4 +8,5 @@ public class Images {
     public static final Image SourcerA = new Image("/saucer1a.ico", 50, 40, false, false); // Enemy 1
     public static final Image SourcerB = new Image("/saucer2a.ico", 50, 40, false, false); // Enemy 2
     public static final Image SourcerC = new Image("/saucer3a.ico", 50, 40, false, false); // Enemy 3
+    public static final Image Wall = new Image("/wall.png", 40, 30, false, false); 
 }
